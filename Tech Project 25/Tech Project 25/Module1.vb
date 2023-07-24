583ce778b5b145b9b5479625aa79d37dc0d5c3d8
Aditha Dinuja Serasinghe <58882366+adserasinghe@users.noreply.github.com>
2023-07-24 13:10:24 +0530

    text files in visual basic files

---
+++ Tech Project 25/Tech Project 25/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+        '(0) is "David", (1) is "Peter"
+        Console.WriteLine("(0) and (1) are good friend ", "David", "Peter")
+        '(1) is used multiple times,
+        Console.WriteLine("{1} * {1} ={0}", 9 * 9, 9)
+        Console.ReadLine()
+    End Sub
+
+End Module
