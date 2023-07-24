26ba026e6a13e403b8ad14aa3475c3c28e9f3dc4
Aditha Dinuja Serasinghe <58882366+adserasinghe@users.noreply.github.com>
2023-07-24 13:12:10 +0530

    projects in console project

---
+++ Tech project 24/Tech project 24/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Console writeline result")
+        Console.WriteLine("========================")
+        Console.WriteLine("Iteam 1")
+        Console.WriteLine(2.345)
+        Console.WriteLine("Iteam 3")
+        Console.WriteLine("Iteam 4")
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine()
+        Console.WriteLine("Console Write result")
+        Console.WriteLine("======================")
+        Console.WriteLine("Iteam 12.345Iteam 3Iteam 4")
+        Console.ReadLine()
+    End Sub
+
+End Module
